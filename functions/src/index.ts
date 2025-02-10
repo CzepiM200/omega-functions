e59f210ae27af53361e42f2b4f667762b6eceee0
CzepiM200 <czepimm200@gmail.com>
2025-02-10 22:03:13 +0100

    Add on register function

---
+++ functions/src/index.ts
@@ -0,0 +1,14 @@
+import * as auth from "firebase-functions/v1/auth";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+export const setUserRoleOnSignup = auth.user().onCreate(async (user) => {
+  const userEmail = user.email ?? "";
+
+  await admin.firestore().collection("users").doc(user.uid).set({
+    email: userEmail,
+    role: "user",
+    name: userEmail,
+  });
+});
